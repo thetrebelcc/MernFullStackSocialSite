b14d980077ee89589e13c2d12deedd4b7a98bd6e
Fabian Anguiano <fabiananguiano@gmail.com>
2018-11-03 12:05:07 -0500

    working on email validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -35,7 +35,7 @@ User.findOne({ email: req.body.email })
 
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if(err) throw errl
+                if(err) throw err
                 newUser.password = hash;
                 newUser.save()
                 .then(user => res.json(user))
@@ -45,6 +45,30 @@ User.findOne({ email: req.body.email })
         })
        
     }
+})
+});
+
+
+router.post('/login', (req,res) => {
+const email = req.body.email;
+const password = req.body.password;
+
+// find used by email 
+User.findOne({email})
+.then(user => {
+    if(!user){
+        return res.status(404).json({email:'User not found'})
+    }
+
+    // check password
+bcrypt.compare(password, user.password).then(isMatch => {
+    if(isMatch){
+        res.json({ mes: 'Success'});
+    } else{
+        return res.status(400).json({ password: 'Password wrong dummy'})
+    }
+})
+     
 })
 });
 
