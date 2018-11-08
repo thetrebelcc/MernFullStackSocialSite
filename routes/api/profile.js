859d3206f70c6c3f29ac88b1bc936cf810535c5b
Fabian Anguiano <fabiananguiano@gmail.com>
2018-11-08 14:30:02 -0600

    Route work for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Loads profile models
+
+
+// Load user profile
+
+
+
+
+
+
+
 
 router.get('/test', (req, res) => res.json({ msg: "Profile works" }));
 
