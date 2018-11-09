18ce89e137d4eee8c8ce9b8d5d53d9a5080b0eea
Fabian Anguiano <fabiananguiano@gmail.com>
2018-11-09 14:31:08 -0600

    more profile route work

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,17 +5,34 @@ const passport = require('passport');
 
 // Loads profile models
 
+const Profile = require('../../models/Profile'); 
 
 // Load user profile
+const User = require('../../models/User'); 
 
 
 
 
-
-
-
-
+// @route GET API / Profile
+// @desc Test profile route
+// @access public
 router.get('/test', (req, res) => res.json({ msg: "Profile works" }));
+
+// @route GET API/Profile
+// @desc Test profile route
+// @access private
+router.get('/', passport.authenticate('jwt', {session: false}), (req,res) => {
+Profile.findOne({user: req.user.id})
+.then(profile => {
+    if(!profile){
+        errors.noprofile = 'There is no profile';
+        return res.status(404).json(errors);
+    }
+    res.json(profile);
+})
+
+});
+
 
 
 module.exports = router;
